89b276dfe36d2d7f3221323a19bf5964e3b939a7
austin <bomar289@gmail.com>
2022-11-28 18:55:20 -0800

    part 3, task scheduling

--- build/esp-idf/mbedtls/mbedtls/library/version_features.c
+++
@@ -1,1 +0,0 @@
-/home/austin/esp/esp-idf/components/mbedtls/mbedtls/library/version_features.c
--- main/test_freertos_main.c
+++ main/test_freertos_main.c
@@ -13,10 +13,25 @@ static const BaseType_t app_cpu = 1;
 #endif
 
 #define BLINK_PERIOD 1000
-#define SECRET_BLINK_PERIOD 300
+#define SECRET_BLINK_PERIOD 250
 
 static const int led_pin = 17;
 static uint8_t s_led_state = 0;
+
+static TaskHandle_t task1;
+static TaskHandle_t task2;
+
+
+/* 
+Introduction to RTOS part 3: Task Scheduling
+https://www.youtube.com/watch?v=95yUbClyf3E&list=PLEBQazB0HUyQ4hAPU1cJED6t3DU0h34bz&index=4
+
+In this example we create 2 tasks that print stuff. A third task will periodically suspend
+and resume one of them. Finally it deletes said task. You can see all of that happen via 
+its output on the console.
+
+*/
+
 
 
 static void configure_led(void)
@@ -27,7 +42,16 @@ static void configure_led(void)
 }
 
 
-void blink_led(void *parameter)
+void setUp(void)
+{
+    /* Configure the peripheral according to the LED type */
+    configure_led();
+
+}
+
+
+
+void starttask1(void *parameter)
 {
     /* Set the GPIO level according to the state (LOW or HIGH)*/    
     while (1) {
@@ -40,7 +64,7 @@ void blink_led(void *parameter)
 }
 
 
-void secret_blink_led(void *parameter)
+void starttask2(void *parameter)
 {
     /* Set the GPIO level according to the state (LOW or HIGH)*/    
     while (1) {
@@ -53,14 +77,6 @@ void secret_blink_led(void *parameter)
 }
 
 
-void setUp(void)
-{
-    /* Configure the peripheral according to the LED type */
-    configure_led();
-
-
-}
-
 
 void app_main(void)
 {
@@ -68,23 +84,39 @@ void app_main(void)
     setUp();
 
     xTaskCreatePinnedToCore(
-        blink_led,
+        starttask1,
         "blink led",
         1024,
         NULL,
         1,
-        NULL,
+        &task1,
         app_cpu
     );
 
     xTaskCreatePinnedToCore(
-        secret_blink_led,
+        starttask2,
         "secret blink led",
         1024,
         NULL,
         1,
-        NULL,
+        &task2,
         app_cpu
     );
 
+
+    for(int i=0; i<3; i++){
+        vTaskSuspend(task2);
+        vTaskDelay(2000 / portTICK_PERIOD_MS);
+        vTaskResume(task2);
+        vTaskDelay(2000 / portTICK_PERIOD_MS);
+    }
+
+    if(task2 != NULL){
+        vTaskDelete(task2);
+        task2 = NULL;
+    }
+    
+    while(1){
+        vTaskDelay(1000 / portTICK_PERIOD_MS);
+    }
 }
